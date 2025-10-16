16f2a08c1ab00501948a66e368dd92efcc40d7be
Eloote <100208992+Eloote@users.noreply.github.com>
2025-10-16 20:20:18 +0300

    1 var Lyushin EA.py

--- 1lab/main.py
+++ 1lab/main.py
@@ -1,34 +1,35 @@
-
-
 if __name__ == "__main__":
-    slova_otv = {}
+    work_otv = {}
     byk_otv = {}
-    
+
     S = input()
-    
+
+
     def schet(n, mass):
-        for i in n:
-            if i not in mass:
-                mass[i] = 1
+        for ind in n:
+            if ind not in mass:
+                mass[ind] = 1
             else:
-                mass[i] += 1
+                mass[ind] += 1
         return mass
-    
-    slova = S
-    byk = S.replace(" ", "")
-    for i in '0123456789':
-        slova = slova.replace(i, " ")
-        byk = byk.replace(i, "")
-    slova = slova.split()
-    
-    slova_otv = schet(slova, slova_otv)
+
+
+    word = S
+    byk = S.replace(' ', '')
+    for i in '0123456789,.;:!&?':
+        word = word.replace(i, ' ')
+        byk = byk.replace(i, '')
+    word = word.split()
+
+    work_otv = schet(word, work_otv)
     byk_otv = schet(byk, byk_otv)
-    
-    print(f"Количество слов: {slova_otv}")
+
+    print(f"Количество слов: {work_otv}")
     print(f"Количество букв: {byk_otv}")
-    
-    top_slov = sorted(slova_otv.items(), key=lambda item: item[1], reverse=True)[0:5]
+
+    top_slov = sorted(work_otv.items(), key=lambda item: item[1], reverse=True)[0:5]
     print(f"5 самых популярных слов: {top_slov}")
     top_byk = sorted(byk_otv.items(), key=lambda item: item[1], reverse=True)[0:5]
     print(f"5 самых популярных букв: {top_byk}")
-    pass # Ваш код здесь
+
+    pass  # Ваш код здесь
