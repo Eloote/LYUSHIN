72d955ea11abe34df3340dcd7fd16af9fbd04724
Eloote <100208992+Eloote@users.noreply.github.com>
2025-10-16 20:04:16 +0300

    1 variatn Lyushin EA PI25-2.py

--- 1lab/main.py
+++ 1lab/main.py
@@ -1,4 +1,34 @@
 
 
 if __name__ == "__main__":
+    slova_otv = {}
+    byk_otv = {}
+    
+    S = input()
+    
+    def schet(n, mass):
+        for i in n:
+            if i not in mass:
+                mass[i] = 1
+            else:
+                mass[i] += 1
+        return mass
+    
+    slova = S
+    byk = S.replace(" ", "")
+    for i in '0123456789':
+        slova = slova.replace(i, " ")
+        byk = byk.replace(i, "")
+    slova = slova.split()
+    
+    slova_otv = schet(slova, slova_otv)
+    byk_otv = schet(byk, byk_otv)
+    
+    print(f"Количество слов: {slova_otv}")
+    print(f"Количество букв: {byk_otv}")
+    
+    top_slov = sorted(slova_otv.items(), key=lambda item: item[1], reverse=True)[0:5]
+    print(f"5 самых популярных слов: {top_slov}")
+    top_byk = sorted(byk_otv.items(), key=lambda item: item[1], reverse=True)[0:5]
+    print(f"5 самых популярных букв: {top_byk}")
     pass # Ваш код здесь
