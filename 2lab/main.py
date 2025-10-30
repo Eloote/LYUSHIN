9309333b4be9e7536ec73e802d120319220cd44d
Eloote <100208992+Eloote@users.noreply.github.com>
2025-10-30 19:40:48 +0300

    1 вариант.py

--- 2lab/main.py
+++ 2lab/main.py
@@ -1,4 +1,38 @@
-
-
 if __name__ == "__main__":
-    pass # Ваш код здесь
+    small = 'abcdefghijklmnopqrstuvwxyz'
+    big = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
+    def proverka():
+        line = input()
+        vse = small + big
+        for i in line:
+            if i not in vse:
+                print('Введите только латинские буквы')
+                return proverka()
+        return line
+
+
+    def rem(n):
+        delet = set()
+
+        for i in range(26):
+            small_cnt = n.count(small[i])
+            big_cnt = n.count(big[i])
+
+            if small_cnt > big_cnt:
+                delet.add(small[i])
+                delet.add(big[i])
+
+        res = ''
+        for q in n:
+            if q not in delet:
+                res += q
+
+        return res
+
+
+    k = proverka()
+    print(rem(k))
+
+    pass  # Ваш код здесь
